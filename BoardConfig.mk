8d2db761a4548868933b77a5936299548395b7e9
Paul Beeler <pbeeler80@gmail.com>
2015-03-03 22:11:03 -0700

    Seperate kernel toolchain from ROM toolchain
    TARGET_SM_KERNEL should be defined.
    
    Change-Id: If1ad87f8ace1bd10299a2192df3726cf97e4eaba
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,10 +38,11 @@ TARGET_NO_BOOTLOADER := true
 
 TARGET_NO_RADIOIMAGE := true
 
-ifeq ($(HOST_OS),linux)
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+# Inline kernel building
+ifndef TARGET_SM_KERNEL
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 else
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-$(TARGET_SM_KERNEL)/bin
 endif
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
