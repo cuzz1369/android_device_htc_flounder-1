2bafbea220ac5402c8b36a5e404b65ec5c4f1e40
Tim Klein <cuzz1369@gmail.com>
2015-04-05 14:31:54 -0400

    Update and rename cm.mk to candy5.mk

--- candy5.mk
+++ candy5.mk
@@ -3,10 +3,10 @@ TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/candy5/config/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/candy5/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
@@ -14,6 +14,6 @@ $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
 $(call inherit-product-if-exists, vendor/htc/flounder/flounder-vendor-blobs.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_flounder
+PRODUCT_NAME := candy5_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
